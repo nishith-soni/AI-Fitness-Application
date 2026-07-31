07838be47beca633c842ef96cc8ea12f93f43ae4
nishith-soni <nishithsoni4444@gmail.com>
2026-07-31 01:49:12 -0400

    Add activity service with tracking functionality and MongoDB integration

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    SWIMMING,
+    STRETCHING,
+    CYCLING,
+    YOGA,
+    WEIGHT_TRAINING,
+    CARDIO,
+    OTHER
+}
