6d7abb0a32cdc5397f196e799abc9ca50f0e7f79
nishith-soni <nishithsoni4444@gmail.com>
2026-09-12 19:38:56 -0400

    feat: implement activity detail and list components with data fetching and display

--- frontend/src/components/ActivityList.jsx
+++ frontend/src/components/ActivityList.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { Card, CardContent, Grid, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router';
 
 const ActivityList = () => {
+
+    const [activities, setActivities] = useState([]);
+    const navigate = useNavigate();
+
+    const fetchActivities = async () => {
+        try {
+            const response = await getActivities();
+            setActivities(response.data);
+        } catch (error) {
+            console.error('Error fetching activities:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchActivities();
+    }, []);
+
     return (
-        <div>Activity List</div>
+        <Grid container spacing={2}>
+            {activities.map((activity) => (
+                <Grid item key={activity.id} container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    <Card sx={{ cursor: 'pointer' }} onClick={() => navigate(`/activities/${activity.id}`)}>
+                        <CardContent>
+                            <Typography variant="h6">{activity.type}</Typography>
+                            <Typography>Duration: {activity.duration} minutes</Typography>
+                            <Typography>Calories Burned: {activity.caloriesBurned}</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
+        </Grid>
     )
 }
 
